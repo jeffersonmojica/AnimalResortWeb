f60511afe9625fa1e7fb436888083d24fb59345b
Gabriiello <gbrielbeltran22@gmail.com>
2024-05-19 22:32:06 -0500

    Loggeo arreglado

---
+++ controladorGoogle.php
@@ -0,0 +1,57 @@
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+<?php
+require_once 'vendor/autoload.php';
+
+session_start();
+
+$client = new Google_Client();
+$client->setClientId('631022063904-0n6jn3vcl53bi1gp1urp0o721ef0q2p0.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://animalresort.com.co/services/controladorGoogle.php');
+
+if (isset($_GET['code'])) {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    if (isset($token['error'])) {
+        die('Error fetching access token: ' . $token['error']);
+    }
+    $client->setAccessToken($token['access_token']);
+
+    // Obtener el perfil del usuario
+    $oauth2 = new Google_Service_Oauth2($client);
+    $userInfo = $oauth2->userinfo->get();
+    $email = $userInfo->email;
+
+    // Conectar a la base de datos
+    
+
+    $conexion = new mysqli("localhost", "animalre", "y367}A]y){K4Cg4", "animalre_database");
+
+    // Verificar la conexión
+    if ($conexion->connect_error) {
+        die('Error de conexión: ' . $conexion->connect_error);
+    }
+
+    $usuario = $conexion->real_escape_string($email);
+
+    $sql = $conexion->query("SELECT * FROM Usuarios WHERE email='$usuario'");
+
+    if ($datos = $sql->fetch_object()) {
+        setcookie('usuario_id', $datos->id, time() + 3600, '/'); // 'usuario_id' es el nombre de la cookie, y 3600 es la duración en segundos
+        header("Location: inicio.html");
+        exit();
+    } else {
+       echo "<script>
+                alert('Usuario no encontrado, Por favor entre con una cuenta registrada');
+                window.location.href = 'index.php';
+              </script>";
+        
+    }
+
+    $conexion->close();
+} else {
+    echo "No se pudo autenticar al usuario.";
+}
+?>
--- index.php
+++ index.php
@@ -1,3 +1,19 @@
+<?php
+require_once 'vendor/autoload.php';
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+session_start();
+
+$client = new Google_Client();
+$client->setClientId('631022063904-0n6jn3vcl53bi1gp1urp0o721ef0q2p0.apps.googleusercontent.com');
+$client->setClientSecret('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');
+$client->setRedirectUri('https://animalresort.com.co/services/controladorGoogle.php');
+$client->addScope("email");
+
+$authUrl = $client->createAuthUrl();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -6,11 +22,38 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <style>
         body {
             background: #9dcd55;
             background: linear-gradient(to right, #3c8735, #9dcd55);
         }
+        /* Estilo para el bot®Æn de inicio de sesi®Æn */
+    .center-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 20px; /* Ajusta el margen superior seg®≤n sea necesario */
+   }
+    .google-login-btn {
+      background-color: #35e850;
+      color: #fff;
+      text-decoration: none;
+      display: inline-flex;
+      align-items: center;
+      padding: 10px 15px;
+      border-radius: 5px;
+      font-size: 16px;
+      transition: background-color 0.3s;
+    }
+
+    .google-login-btn:hover {
+      background-color: #87c190;
+    }
+
+    .google-icon {
+      margin-right: 10px;
+    }
     </style>
 </head>
 
@@ -26,27 +69,43 @@
                 <h2 class="fw-bold text-center py-5">Bienvenido</h2>
 
                 <form action="" method="post">
+                    
+                    <div>
+                         <?php
+                          include("conexion.php");
+                         include("controlador.php");
+                        ?>
 
-                    <?php
-                    include("conexion.php");
-                    include("controlador.php");
-                    ?>
+                         <div class="mb-4">
+                             <label for="email" class="form-label">Correo electronico</label>
+                             <input type="email" class="form-control" name="email">
+                          </div>
 
-                    <div class="mb-4">
-                        <label for="email" class="form-label">Correo electronico</label>
-                        <input type="email" class="form-control" name="email">
+                         <div class="d-grid">
+                            <input name="btn" class="btn btn-success" type="submit" value="Iniciar sesion">
+                         </div>
+
+                    
+
+                         <div class="center-container">
+                             <?php
+                          include("conexion.php");
+                        ?>
+                            <div class="google-login-btn">
+                             
+                             <a href="<?php echo $authUrl; ?>" class="google-login-btn">
+                              <span class="google-icon"><i class="material-icons">account_circle</i></span>
+                              Iniciar sesion con Google
+                             </a>
+                            </div>
+                         </div>
                     </div>
 
-                    <div class="d-grid">
-                        <input name="btn" class="btn btn-success" type="submit" value="Iniciar sesion">
-                    </div>
-
-
-                    <div class="enlace">
-                     <img src="imagenes/ui.svg">
-                     <?php require ('controlador.php')?>
-                    <a href="<?php echo $client->createAuthUrl() ?>">Iniciar sesi√≥n con Google</a>
-                   </div>
+                   
+                   
+                   
+                   
+                   
 
                 </form>
 
